12f713552da73e6f4034db2bffc49f7a2a779bd8
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-08-30 17:00:14 -0400

    Remove app and data

--- MVCData/dbo/Tables/Student.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Student]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [StudentId] INT NOT NULL, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL
-)
